223fe1de289b95a6cd99698152eb9ceb33d55ac3
Das <athuldas@hamon.in>
2022-10-01 20:38:56 +0530

    Adds Dummy data.

--- expense-tracker/src/components/ExpenseItem/index.js
+++ expense-tracker/src/components/ExpenseItem/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ExpenseItemStyled from './style';
 
-function ExpenseItem() {
-    return <ExpenseItemStyled>ExpenseItem</ExpenseItemStyled>;
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
+
+    return (
+        <ExpenseItemStyled>
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </ExpenseItemStyled>
+    );
 }
 
 export default ExpenseItem;
--- expense-tracker/src/components/ExpenseItem/style.js
+++ expense-tracker/src/components/ExpenseItem/style.js
@@ -1,10 +1,71 @@
 import styled from 'styled-components';
 
 const ExpenseItemStyled = styled.div`
-    background: #3f3f3f;
-    text-align: center;
-    width: 100vw;
-    height: 100vh;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.5);
+    padding: 0.5rem;
+    margin: 1rem 0;
+    gap: 1rem;
+    border-radius: 12px;
+    background-color: #4b4b4b;
+
+    .expense-item__description {
+        // display: flex;
+        // flex-direction: column;
+        // gap: 1rem;
+        // align-items: flex-end;
+        // flex-flow: column-reverse;
+        // justify-content: flex-start;
+        // flex: 1;
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        flex: 1;
+
+        .expense-item h2 {
+            font-size: 1rem;
+            flex: 1;
+            margin: 0 1rem;
+            color: white;
+        }
+
+        h2 {
+            color: white;
+        }
+
+        .expense-item__price {
+            font-size: 1rem;
+            font-weight: bold;
+            color: white;
+            background-color: #40005d;
+            border: 1px solid white;
+            padding: 0.5rem;
+            border-radius: 12px;
+        }
+    }
+
+    // @media (min-width: 580px) {
+    //     .expense-item__description {
+    //         display: flex;
+    //         flex-direction: row;
+    //         align-items: center;
+    //         justify-content: flex-start;
+    //         flex: 1;
+    //     }
+
+    //     .expense-item__description h2 {
+    //         font-size: 1.25rem;
+    //     }
+
+    //     .expense-item__price {
+    //         font-size: 1.25rem;
+    //         padding: 0.5rem 1.5rem;
+    //     }
+    // }
 `;
 
 export default ExpenseItemStyled;
