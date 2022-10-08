0315e9be300d6fa061339aa8d9f8873df89993f7
Das <athuldas@hamon.in>
2022-10-08 11:29:51 +0530

    Initial commit.

---
+++ add-random-person/src/App.js
@@ -0,0 +1,11 @@
+import AddUser from './components/AddUser.js';
+
+function App() {
+    return (
+        <div>
+            <AddUser />
+        </div>
+    );
+}
+
+export default App;
---
+++ add-random-person/src/components/AddUser.js/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Card from '../Card';
+
+const AddUser = () => {
+    const addUserhandler = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <Card>
+            <form onSubmit={addUserhandler}>
+                <label htmlFor="username">UserName</label>
+                <input id="username" type="text" />
+                <label htmlFor="age">Age</label>
+                <input id="age" type="number" />
+                <button type="button">Add User</button>
+            </form>
+        </Card>
+    );
+};
+
+export default AddUser;
