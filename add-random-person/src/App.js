9ae6525c80defe5e86be5b8cdafefeb659e6944b
Das <athuldas@hamon.in>
2022-10-08 19:15:35 +0530

    Adds React Portals.

--- add-random-person/src/App.js
+++ add-random-person/src/App.js
@@ -3,28 +3,28 @@ import AddUser from './components/AddUser/Adduser';
 import UserList from './components/UserList/UserList';
 
 function App() {
-    const [userList, setUserList] = useState([]);
-    const addUserHandler = (userName, userAge) => {
-        if (userName && userAge) {
-            setUserList((prevList) => {
-                return [
-                    ...prevList,
-                    {
-                        id: Math.round(Math.random() * 100).toString(),
-                        name: userName,
-                        age: userAge,
-                    },
-                ];
-            });
-        }
-    };
+  const [userList, setUserList] = useState([]);
+  const addUserHandler = (userName, userAge) => {
+    if (userName && userAge) {
+      setUserList((prevList) => {
+        return [
+          ...prevList,
+          {
+            id: Math.round(Math.random() * 100).toString(),
+            name: userName,
+            age: userAge,
+          },
+        ];
+      });
+    }
+  };
 
-    return (
-        <div>
-            <AddUser onAddUser={addUserHandler} />
-            {userList.length && <UserList users={userList} />}
-        </div>
-    );
+  return (
+    <>
+      <AddUser onAddUser={addUserHandler} />
+      {userList.length > 0 && <UserList users={userList} />}
+    </>
+  );
 }
 
 export default App;
--- add-random-person/src/components/ErrorModal/ErrorModal.js
+++ add-random-person/src/components/ErrorModal/ErrorModal.js
@@ -1,23 +1,43 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Card from '../Card/Card';
 import Button from '../Button/Button';
 import styles from './ErrorModal.module.css';
 
 const ErrorModal = (props) => {
+  const Backdrop = (props) => {
+    return <div className={styles.backdrop} onClick={props.onConfirm} />;
+  };
+
+  const ModalOverlay = (props) => {
     return (
-        <>
-            <div className={styles.backdrop} onClick={props.onConfirm} />
-            <Card className={styles.modal}>
-                <header className={styles.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={styles.content}>{props.message}</div>
-                <footer className={styles.actions}>
-                    <Button onClick={props.onConfirm}>Okay</Button>
-                </footer>
-            </Card>
-        </>
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={styles.content}>{props.message}</div>
+        <footer className={styles.actions}>
+          <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+      </Card>
     );
+  };
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById('overlay-root')
+      )}
+    </>
+  );
 };
 
 export default ErrorModal;
