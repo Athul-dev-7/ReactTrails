76571e0d40ea0981c1d9a23d24f26217a27e0e15
Das <athuldas@hamon.in>
2022-11-03 11:21:02 +0530

    Adds Cart.

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -1,10 +1,23 @@
+import React, { useState } from 'react';
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 function App() {
+    const [cartIsShwon, setCartIsShown] = useState(false);
+
+    const showCartHandler = () => {
+        setCartIsShown(true);
+    };
+
+    const hideCartHandler = () => {
+        setCartIsShown(false);
+    };
+
     return (
         <>
-            <Header />
+            {cartIsShwon && <Cart onHideCart={hideCartHandler} />}
+            <Header onShowCart={showCartHandler} />
             <main>
                 <Meals />
             </main>
