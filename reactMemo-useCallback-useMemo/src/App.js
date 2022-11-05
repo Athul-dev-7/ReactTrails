301b9d504b357a03e69b941e098c560db10033b3
Das <athuldas@hamon.in>
2022-11-05 11:22:46 +0530

    Onclicking the button, we can see the DOM changes on <p>, when the state is true  and <div> changes when the state is false.

--- reactMemo-useCallback-useMemo/src/App.js
+++ reactMemo-useCallback-useMemo/src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Button from './components/UI/Button/Button';
 import './App.css';
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+    const [showParagraph, setShowParagraph] = useState(false);
+
+    console.log('APP RUNNING');
+
+    const toggleParagraphHandler = () => {
+        setShowParagraph((prevState) => !prevState);
+    };
+
+    /**
+     Onclicking the button, we can see the DOM changes on <p>, when the state is true  and <div> changes when the state is false.
+     *
+     */
+
+    return (
+        <div className="app">
+            <h1>Hi there!</h1>
+            {showParagraph && <p>This is new!</p>}
+            <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+        </div>
+    );
 }
 
 export default App;
