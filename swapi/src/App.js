7bc81d2b57dc07ce04aac66fa790d2c5765eb7e5
Das <athuldas@hamon.in>
2022-11-06 11:45:19 +0530

    Adds async/await instead of then.

--- swapi/src/App.js
+++ swapi/src/App.js
@@ -6,22 +6,18 @@ import './App.css';
 function App() {
     const [movies, setMovies] = useState([]);
 
-    const fetchMoviesHandler = () => {
-        fetch('https://swapi.dev/api/films')
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                const newMovieList = data.results.map((movieData) => {
-                    return {
-                        id: movieData.episode_id,
-                        title: movieData.title,
-                        releaseDate: movieData.release_date,
-                        openingText: movieData.opening_crawl,
-                    };
-                });
-                setMovies(newMovieList);
-            });
+    const fetchMoviesHandler = async () => {
+        const response = await fetch('https://swapi.dev/api/films');
+        const data = await response.json();
+        const newMovieList = data.results.map((movieData) => {
+            return {
+                id: movieData.episode_id,
+                title: movieData.title,
+                releaseDate: movieData.release_date,
+                openingText: movieData.opening_crawl,
+            };
+        });
+        setMovies(newMovieList);
     };
 
     return (
