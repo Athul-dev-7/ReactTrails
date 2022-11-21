4dffbff5ee8b1cd68d9e6987433779d8c2971829
Das <athuldas@hamon.in>
2022-11-21 10:06:01 +0530

    Adds useRef and useState for inputHandler.

--- user-forms/src/components/SimpleInput.js
+++ user-forms/src/components/SimpleInput.js
@@ -1,9 +1,28 @@
+import { useState, useRef } from 'react';
 const SimpleInput = (props) => {
+    const [enteredName, setEnteredName] = useState('');
+    const nameInputRef = useRef();
+
+    const formSubmissionHandler = (event) => {
+        event.preventdefault();
+        setEnteredName(nameInputRef.current.value);
+        setEnteredName('');
+    };
+    const inputChangeHandler = (event) => {
+        setEnteredName(event.target.value);
+    };
+
     return (
-        <form>
+        <form onSubmit={formSubmissionHandler}>
             <div className="form-control">
                 <label htmlFor="name">Your Name</label>
-                <input type="text" id="name" />
+                <input
+                    ref={nameInputRef}
+                    type="text"
+                    id="name"
+                    onChange={inputChangeHandler}
+                    value={enteredName}
+                />
             </div>
             <div className="form-actions">
                 <button>Submit</button>
