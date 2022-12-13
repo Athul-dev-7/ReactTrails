f3157bc22ecddad4bb9f66abab7a2bcee31e314d
Das <athuldas@hamon.in>
2022-12-13 16:14:55 +0530

    Adds folders for each slice

--- redux-basic-project/src/store/index.js
+++ redux-basic-project/src/store/index.js
@@ -1,46 +1,9 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
-
-const initialCounterState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-    name: 'counter',
-    initialState: initialCounterState,
-    reducers: {
-        increment(prevState) {
-            prevState.counter++;
-        },
-        decrement(prevState) {
-            prevState.counter--;
-        },
-        increase(prevState, action) {
-            prevState.counter = prevState.counter + action.payload;
-        },
-        toggle(prevState) {
-            prevState.showCounter = !prevState.showCounter;
-        },
-    },
-});
-
-const initialAuthState = { isAuthenticated: false };
-
-const authSlice = createSlice({
-    name: 'authentication',
-    initialState: initialAuthState,
-    reducers: {
-        login(prevState) {
-            prevState.isAuthenticated = true;
-        },
-        logout(prevState) {
-            prevState.isAuthenticated = false;
-        },
-    },
-});
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counter-store/counter';
+import authReducer from './auth-store/auth';
 
 const store = configureStore({
-    reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+    reducer: { counter: counterReducer, auth: authReducer },
 });
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
