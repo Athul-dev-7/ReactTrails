e171fca7b952777a051b1c46bdca5356a6c3a67f
Das <athuldas@hamon.in>
2022-12-13 15:41:58 +0530

    Adds seperate slices

--- redux-basic-project/src/store/index.js
+++ redux-basic-project/src/store/index.js
@@ -21,10 +21,26 @@ const counterSlice = createSlice({
     },
 });
 
+const initialAuthState = { isAuthenticated: false };
+
+const authSlice = createSlice({
+    name: 'authentication',
+    initialState: initialAuthState,
+    reducers: {
+        login(prevState) {
+            prevState.isAuthenticated = true;
+        },
+        logout(prevState) {
+            prevState.isAuthenticated = false;
+        },
+    },
+});
+
 const store = configureStore({
-    reducer: counterSlice.reducer,
+    reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
