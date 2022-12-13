2f3396b35bcc51fd0ce02cef46feb6b3bea3bdfb
Das <athuldas@hamon.in>
2022-12-13 13:36:56 +0530

    Adds useSelector & useDispatch

--- redux-basic-project/src/store/index.js
+++ redux-basic-project/src/store/index.js
@@ -1,17 +1,34 @@
 import { createStore } from 'redux';
 
-const counterReducer = (prevState = { counter: 0 }, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: prevState.counter + 1,
-        };
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (prevState = initialState, action) => {
+    switch (action.type) {
+        case 'increment':
+            return {
+                counter: prevState.counter + 1,
+                showCounter: prevState.showCounter,
+            };
+
+        case 'decrement':
+            return {
+                counter: prevState.counter - 1,
+                showCounter: prevState.showCounter,
+            };
+        case 'increase':
+            return {
+                counter: prevState.counter + action.amount,
+                showCounter: prevState.showCounter,
+            };
+        case 'toggle':
+            return {
+                counter: prevState.counter,
+                showCounter: !prevState.showCounter,
+            };
+
+        default:
+            return prevState;
     }
-    if (action.type === 'decrement') {
-        return {
-            counter: prevState.counter - 1,
-        };
-    }
-    return prevState;
 };
 
 const store = createStore(counterReducer);
