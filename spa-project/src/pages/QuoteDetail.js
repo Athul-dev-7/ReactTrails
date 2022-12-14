2ddc6e25a9fc240214a32d0acbf99e29597f061c
Das <athuldas@hamon.in>
2022-12-14 18:26:52 +0530

    Adds Nested-Route.

--- spa-project/src/pages/QuoteDetail.js
+++ spa-project/src/pages/QuoteDetail.js
@@ -1,11 +1,14 @@
-import { useParams } from 'react-router-dom';
-
+import { Route, useParams } from 'react-router-dom';
+import Comments from '../components/comments/Comments';
 const QuoteDetail = () => {
     const params = useParams();
     return (
         <section>
             <h1>Quote Detail Page</h1>
             <p>{params.quoteId}</p>
+            <Route path={`/quotes/${params.quoteId}/comments`}>
+                <Comments />
+            </Route>
         </section>
     );
 };
