0fbac05a30b791d7894486c9612415a3697e2856
Das <athuldas@hamon.in>
2022-12-15 11:27:55 +0530

    Adds DUMMY_DATA.

--- spa-project/src/pages/AllQuotes.js
+++ spa-project/src/pages/AllQuotes.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import QuoteList from '../components/quotes/QuoteList';
 
+const DUMMY_QUOTES = [
+    {
+        id: 'q1',
+        author: 'Athul',
+        text: 'Learning React is fun!',
+    },
+    {
+        id: 'q2',
+        author: 'Pranav',
+        text: 'Learning React is great!',
+    },
+];
 const AllQuotes = () => {
-    return <h1>All Quotes Page</h1>;
+    return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
