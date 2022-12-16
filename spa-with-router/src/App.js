0cd234797fe61957f507544ea7daa755f5debe17
Das <athuldas@hamon.in>
2022-12-16 12:20:04 +0530

    Updates to react-router v6.

--- spa-with-router/src/App.js
+++ spa-with-router/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
 import MainHeader from './components/MainHeader/MainHeader';
@@ -9,20 +9,23 @@ function App() {
         <div>
             <MainHeader />
             <main>
-                <Switch>
-                    <Route path="/" exact>
-                        <Redirect to="/welcome" />
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Navigate replace to="/welcome" />}
+                    />
+                    <Route path="/welcome" element={<Welcome />}>
+                        <Route
+                            path="new-user"
+                            element={<p>Welcome, new user</p>}
+                        />
                     </Route>
-                    <Route path="/welcome">
-                        <Welcome />
-                    </Route>
-                    <Route path="/products" exact>
-                        <Products />
-                    </Route>
-                    <Route path="/products/:productId">
-                        <ProductDetails />
-                    </Route>
-                </Switch>
+                    <Route path="/products" element={<Products />} />
+                    <Route
+                        path="/products/:productId"
+                        element={<ProductDetails />}
+                    />
+                </Routes>
             </main>
         </div>
     );
